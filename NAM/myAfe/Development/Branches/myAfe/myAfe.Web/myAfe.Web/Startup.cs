106652e8385909e0aa466df43efd512c85c84b71
jay <jay@example.com>
2023-04-05 19:04:31 +0530

    files migrated

---
+++ NAM/myAfe/Development/Branches/myAfe/myAfe.Web/myAfe.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myAfe.Web.Startup))]
+namespace myAfe.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
