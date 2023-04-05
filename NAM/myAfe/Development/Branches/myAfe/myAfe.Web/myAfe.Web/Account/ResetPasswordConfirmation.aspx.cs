d0506c8e2a0fa904dd59d47f3e4686a9383a05a2
Administrator <WIN-N322HOSBH2K\Administrator>
2023-04-05 10:26:04 +0000

    website added and referenced other dll in other folder
    git-tfs-id: [http://192.168.3.197:8080/tfs/DefaultCollection]$/Enterprise Applications;C572

---
+++ NAM/myAfe/Development/Branches/myAfe/myAfe.Web/myAfe.Web/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace myAfe.Web.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ NAM/myAfe/Development/Branches/myAfe/myAfe.Web/myAfe.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="myAfe.Web.Global" Language="C#" %>
